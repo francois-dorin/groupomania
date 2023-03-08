c9e3b6a1e6bb252e6982653151f16226e52ed09d
François DORIN <36440144+francois-dorin@users.noreply.github.com>
2023-03-08 21:24:14 +0100

    Update database.js
    Load config for database from environment variables

--- backend/util/database.js
+++ backend/util/database.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 const env = require('dotenv').config()
 
 const database = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password: process.env.password,
-    database: process.env.bdd,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     multipleStatements: true,
   });
   database.connect(function(err){
